0f553101e5c9f7c35da7d33dd7196c2700071808
Derya-C <deryacakiral63@gmail.com>
2024-09-09 23:24:52 +0300

    Add files via upload

---
+++ src 3/Kitap.java
@@ -0,0 +1,32 @@
+
+class Kitap {
+
+    //TODO: int kitapNumarasi ve yayinYili; String kitapAdi ve yazarAd; double fiyat fieldlarini olusturun.
+    // Oluşan her kitap nesnesinin yegane (unique) bir kitap numarası olmalıdır.
+
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////
+
+    public Kitap(String kitapAdi, String yazarAdi, int yayinYili, double fiyat) {
+        //TODO: Kitap nesnesi oluşturma sırasında ilk çalışan metodu (constructor) olusturun
+        //NOT: Her kitabın tekil bir kitap numarası (id) olduğunu unutmayınız...
+
+
+
+
+    }
+
+    // TODO: Gerekli olan getter() veya setter() metodlarını oluşturunuz.
+    // Kullanılmayanları oluşturmanıza gerek yok.
+    // Program tamamlandıktan sonra kullanılmayan metodları sonradan silmek de bir yöntemdir
+
+    @Override
+    public String toString() {
+        //TODO: Kitabın özelliklerini yazdıracak toString() metodunu tamamlayınız...(Return icini doldurun)
+
+        return "...";
+    }
+}
